c5b8fabb4398bf151fc7f19c84340d933d752383
arshaharyoko <arshapermana.h@gmail.com>
2023-10-14 19:29:51 +0700

    added Digest struct

--- cse.hh
+++ cse.hh
@@ -7,16 +7,26 @@
 #define CSE_1024 1024
 #define CSE_2048 2048
 
+#include <iomanip>
 #include <string>
+#include <sstream>
+#include <cmath>
 #include <map>
+#include <immintrin.h>
 #include "fractal.hh"
-#include "matrix_ops.hh"
 
-std::string hex_to_str(const std::string hex);
-std::string str_to_hex(const std::string string);
+struct Digest {
+    std::string hash;
+    double* key;
+};
+
 std::string double_to_hex(const double dec, const uint precision);
-std::string hex_add(const char a, const char b);
+std::string hex_add(const char a, const char b, const uint precision);
+void array_ceil_simd(double* a);
+int ceil_simd(double a);
+int bit_rotate_right(int x, size_t n);
+int bit_rotate_left(int x, size_t n);
 size_t pad_dim_to_divisible(const size_t num, const size_t div);
-std::string cse(const std::string string, const uint32_t dim, const size_t digest_length);
+Digest cse(const std::string string, const uint dim, const size_t digest_length);
 
 #endif
